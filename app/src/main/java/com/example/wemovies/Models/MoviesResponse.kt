37726a71ebe860df089c1af9579c6a03c78b644e
kmkz <eduardomoura228@gmail.com>
2025-05-07 10:39:09 -0300

    Remoçao de arquivos Duplicados

--- app/src/main/java/com/example/wemovies/Models/MoviesResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.wemovies.Models
-
-
-import com.google.gson.annotations.SerializedName
-
-data class MoviesResponse(
-    @SerializedName("products")
-    val movies: List<Movie>
-)
